ecf4ef68766f1baabbe54c9b07ff51fc08f425a6
Rush Weigelt <rush.weigelt@drexel.edu>
2020-01-10 21:53:09 -0500

    a

--- DL_Game/main.js
+++ DL_Game/main.js
@@ -214,7 +214,7 @@ load the model
 async function start(cur_mode) {
 	mode = cur_mode
 	//load model
-	model = await tf.loadLayersModel('model2/model.json')
+	model = await tf.loadLayersModel('model/model.json')
 	
 	//warm up
 	model.predict(tf.zeros([1, 28, 28, 1]))
