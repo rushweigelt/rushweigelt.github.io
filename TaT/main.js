9909c56daca44ffca17b21c6c070948516b29115
Rush Weigelt <rush.weigelt@drexel.edu>
2020-01-14 01:06:32 -0500

    foo

--- TaT/main.js
+++ TaT/main.js
@@ -11,3 +11,9 @@ function Get_User_Hashtag() {
 	var x = document.getElementById("user_hashtag").value;
 	document.getElementById("report_display").innerHTML = x;
 }
+
+function run_NB() {
+	if hash node 2/dev/null; then
+		node NaiveBayes_Gaussian.js 4 2 5 20 20 1 5 3
+	fi
+}
